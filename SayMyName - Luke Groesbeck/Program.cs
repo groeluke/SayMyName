3bea7a7254b19f82fa2a091730f9505e114bc574
lukeg <groeluke@isu.edu>
2026-01-16 08:24:33 -0700

    stash

---
+++ SayMyName - Luke Groesbeck/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Luke Groesbeck
+Project SayMyName
+RCET 2265
+Spring 2026
+Computer Fundamentals and Introduction to Programming
+https://github.com/groeluke/SayMyName.git */
+namespace SayMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name} have a great day!");
+            Console.Read();
+        }
+    }
+}
